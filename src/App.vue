5991b2376bf923bc6754ab2c0ee3051d5440c92e
rodrigoronin <liramailbox@gmail.com>
2020-05-19 13:36:57 -0300

    project base structure

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <Spender />
+    <Footer />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Spender from "./components/Spender";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Header,
+    Footer,
+    Spender,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+body {
+  background-color: #f1f2f3;
+  color: #333;
+  font-weight: bold;
+  font-size: 30px;
+  padding: 0;
+  margin: 0;
+
+  #app {
+    -webkit-font-smoothing: antialiased;
+    text-align: center;
+  }
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header container-fluid">
+    <h1>Spender</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  background: #fff;
+  padding: 0.7em;
+  margin-bottom: 25px;
+}
+</style>
